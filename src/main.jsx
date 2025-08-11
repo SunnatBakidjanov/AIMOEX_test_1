4b288332175bde49dbd5100676df72d710ea4e5b
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-08-11 14:29:11 +0500

    Установил tailwindcss
    ----- tailwindcss -----
    -- Установил tailwindcss
    -- Добавил запись в vite.config.js
    -- Создал index.css + импорт в App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { App } from './app/App.jsx';
 
 const rootElement = document.getElementById('root');
 const reactRoot = createRoot(rootElement);
